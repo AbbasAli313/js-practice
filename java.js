9711e18e03a7d5f7821d95adb8798a015b561e82
AbbasAli313 <abbasalihussaini1212141472@gmail.com>
2024-07-06 10:11:07 -0700

    javascript prictice

---
+++ java.js
@@ -0,0 +1,5 @@
+var number = prompt("Enter a number:");
+for (var i = 1; i <= 10; i++) {
+  var result = number * i;
+  console.log(number, "*", i, "=", result);
+}
---
+++ sum/sum.js
@@ -0,0 +1,7 @@
+var numbers = [];
+for (var i = 0; i < 5; i++) {
+  var userNum = parseFloat(prompt("Enter a number ${i + 1}:"));
+  var num = parseFloat(userNum);
+  userNum.push(numbers);
+}
+console.log(numbers);
